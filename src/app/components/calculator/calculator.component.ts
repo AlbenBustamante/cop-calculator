820d6a3c177890888e0ea3e9253161012e471f72
Alben Bustamante <albenbustamante@gmail.com>
2023-07-20 14:46:03 -0500

    replace: url to a local file

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -11,8 +11,6 @@ import { CalculatorService } from 'src/app/services/calculator.service'
 export class CalculatorComponent implements OnInit {
   coins!: Coin[]
   result!: Result
-  readonly hotWheels: string =
-    'https://github-production-user-asset-6210df.s3.amazonaws.com/81927187/254980991-3fbfcbad-2383-44a9-94de-683235f0e37a.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230720%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230720T185113Z&X-Amz-Expires=300&X-Amz-Signature=9e3c327bb3e1a2cb06ddfe01ff9052f4fc42bb6be11ebb4198b894c53ba3d6ce&X-Amz-SignedHeaders=host&actor_id=81927187&key_id=0&repo_id=514049316'
 
   constructor(private service: CalculatorService) {}
 
